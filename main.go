9015d07cc2108e9f403df65c9df3162f3f70c9f6
Boris Terzic <boris@terzic.be>
2023-03-22 09:56:58 +0100

    Split out import bookmarks code and embed view templates in binary using embed FS

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
+	"embed"
 	"errors"
 	"flag"
 	"fmt"
@@ -10,7 +10,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	"time"
 
 	"github.com/google/uuid"
@@ -57,96 +56,8 @@ func main() {
 	}
 }
 
-type PinboardBookmark []struct {
-	Href        string    `json:"href"`
-	Description string    `json:"description"`
-	Extended    string    `json:"extended"`
-	Meta        string    `json:"meta"`
-	Hash        string    `json:"hash"`
-	Time        time.Time `json:"time"`
-	Shared      string    `json:"shared"`
-	Toread      string    `json:"toread"`
-	Tags        string    `json:"tags"`
-}
-
-var HtmlTagRegex = regexp.MustCompile(`(?s)<[^>]*>(?s)`)
-
-// A go function to remove html tags from a string
-// https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags/1732454#1732454
-func removeHtmlTags(s string) string {
-	return HtmlTagRegex.ReplaceAllString(s, "")
-}
-
-func importBookmarks(importBookmarksJsonFile, importBookmarksUsername string) error {
-	file, err := os.Open(importBookmarksJsonFile)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	var pinboardBookmarks = make(PinboardBookmark, 0)
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&pinboardBookmarks)
-	if err != nil {
-		return err
-	}
-	bookmarks := make([]Bookmark, 0)
-	for _, b := range pinboardBookmarks {
-		bookmark := Bookmark{
-			URL:         b.Href,
-			Title:       b.Description,
-			Description: removeHtmlTags(b.Extended),
-			Tags:        b.Tags,
-			Private:     b.Shared == "no",
-			Created:     b.Time,
-			Updated:     b.Time,
-		}
-		bookmarks = append(bookmarks, bookmark)
-	}
-	log.Println("Importing", len(bookmarks), "bookmarks for user", importBookmarksUsername)
-	// now import all the bookmarks in the database
-	db, err := initAndVerifyDb()
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	// get the user id
-	rows, err := db.Query("SELECT id FROM users WHERE username = ?", importBookmarksUsername)
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
-	if !rows.Next() {
-		return errors.New("user not found")
-	}
-	var userid int
-	err = rows.Scan(&userid)
-	if err != nil {
-		return err
-	}
-	rows.Close()
-	// now insert all the bookmarks
-	stmt, err := db.Prepare("INSERT INTO bookmarks (user_id, url, title, description, tags, private, created, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-	count := 0
-	for _, b := range bookmarks {
-		private := 0
-		if b.Private {
-			private = 1
-		}
-		_, err = stmt.Exec(userid, b.URL, b.Title, b.Description, b.Tags, private, b.Created.Unix(), b.Updated.Unix())
-		if err != nil {
-			return err
-		}
-		count = count + 1
-		if count%25 == 0 {
-			log.Println("Imported", count, "bookmarks")
-		}
-	}
-	return nil
-}
+//go:embed public/views/*.html
+var viewTemplates embed.FS
 
 func runServer() {
 	db, err := initAndVerifyDb()
@@ -158,7 +69,7 @@ func runServer() {
 	e := echo.New()
 
 	t := &Template{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
+		templates: template.Must(template.ParseFS(viewTemplates, "public/views/*.html")),
 	}
 	e.Renderer = t
 
