9e5fe7fb7cf195e06a9b02e69cce673811f30cfc
Boris Terzic <boris@terzic.be>
2023-03-26 10:05:25 +0200

    Adding fts5 module to sqlite in build

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,3 +1,3 @@
 # This will build the program inside a docker container so that it links against an older
 # version of libc and makes it easier to run on somewhat older servers
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.20 go build -v -buildvcs=false
+docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.20 go build -v -buildvcs=false --tags "fts5"
