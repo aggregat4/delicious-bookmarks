16257bc281aebe576a5d9b776e9e6f4a2c0ac99d
Boris Terzic <boris@terzic.be>
2024-01-19 09:47:47 +0100

    fix: removed secondary suppsedly prod build since it does not work. We require CGO_ENABLED to be 1 for sqlite projects to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 go build -v -buildvcs=false --tags "fts5" -o bin/gobookmarks cmd/server/main.go
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -buildvcs=false --tags "fts5" -o bin/gobookmarks-prod cmd/server/main.go
