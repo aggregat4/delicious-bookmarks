5c1dbecb6015f4de3150878c42ebd44d3a2d991e
Boris Terzic <boris@terzic.be>
2024-03-06 09:19:18 +0100

    feature: Add CI and release pipeline and simplify test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,12 +5,12 @@ set -e
 echo "Running tests"
 go test ./...
 
-echo "Checking race conditions"
-go test -race ./... 
+#echo "Checking race conditions"
+#go test -race ./...
 
-echo "Creating coverage report"
-go test -coverprofile=coverage.out ./...
-go tool cover -func coverage.out
-go tool cover -html=coverage.out -o coverage.html
+#echo "Creating coverage report"
+#go test -coverprofile=coverage.out ./...
+#go tool cover -func coverage.out
+#go tool cover -html=coverage.out -o coverage.html
 
 echo "Tests passed"
